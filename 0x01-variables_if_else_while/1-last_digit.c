e109d169f62cd109a51c1439cfe83a81cdd6d22f
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-01 13:14:12 -0700

    print last one digit not two

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,19 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	tmp = n % 100;
+	tmp = n % 10;
 
 	if (tmp > 5)
 	{
-		printf("Last digit of %d is %02d and is greater than 5\n", n, tmp);
+		printf("Last digit of %d is %d and is greater than 5\n", n, tmp);
 	}
 	else if (tmp == 0)
 	{
-		printf("Last digit of %d is %02d and is 0\n", n, tmp);
+		printf("Last digit of %d is %d and is 0\n", n, tmp);
 	}
 	else if (tmp < 6 && tmp != 0)
 	{
-		printf("Last digit of %d is %02d and is less than 6 and not 0\n", n, tmp);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, tmp);
 	}
 
 	return (0);
