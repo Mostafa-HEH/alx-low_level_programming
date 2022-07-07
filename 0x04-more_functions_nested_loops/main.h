f60a80040841c516f5a6f938880ceeb96edd8809
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-07 09:44:03 -0700

    check for uppercase character

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
