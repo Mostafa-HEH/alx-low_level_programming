7f43411a05f73fd52efb604e8c314ffbcf728a2e
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-11 07:44:54 -0700

    print odd numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every odd character of a string
+ * @str: string will be checked
+ *
+ * Reurn: Always nothing
+ */
+void puts2(char *str)
+{
+	int len, i;
+	
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	i = 0;
+	while (i < len)
+	{
+		if ((str[i] % 2) == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	
+	_putchar('\n');
+}
