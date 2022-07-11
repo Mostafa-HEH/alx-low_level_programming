082e99f1f2ae83cc4f0caec601a1ba924d50d3bc
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-11 05:52:51 -0700

    fix for checker

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@ void print_rev(char *s)
 {
 	int i, len;
 
-	len = 0;
+	len = 1;
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
 	i = len;
-	while (i > -1)
+	while (i >= 0)
 	{
 		_putchar(s[i]);
 		i--;
