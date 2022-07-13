58ba303393ce670580e9fd3ed9eb839bfeb6a9c5
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-13 05:36:07 -0700

    concate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
