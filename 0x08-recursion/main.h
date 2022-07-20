6a0159fceb39b5e722974cbd5bb1a9d94d75e1cc
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-20 07:52:03 -0700

    print string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
