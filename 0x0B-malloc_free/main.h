1e0500e01a81ddb6a3e8ed3e78430deed27e6d14
Mostafa-HEH <mostafa813765@gmail.com>
2022-07-25 12:10:01 -0700

    create an array of chars

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
