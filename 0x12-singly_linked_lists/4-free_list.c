ffcc5d6fed34d6c8eb0aba6a9476de14dc04431c
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-08 12:19:58 -0700

    free a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free all list nodes
+ *
+ * Return: Always Nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *p, *q;
+
+	p = head;
+	while (p)
+	{
+		q = p;
+		p = p->next;
+
+		head = p;
+		free(q->str);
+		free(q);
+	}
+}
