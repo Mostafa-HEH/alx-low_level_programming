6028ceb6dadf38da5a8696574d95f08845a2cf3a
Mostafa Hassan <mostafa813765@gmail.com>
2022-10-21 06:10:44 -0700

    convert binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,61 @@
+/**
+ * clc_pow - get power of number
+ * @n: number will get its power
+ * @p: power will rise on
+ *
+ * Return: Always te result.
+ */
+int clc_pow(int n, int p)
+{
+	int result = 1;
+
+	if (p == 0)
+	{
+		result = 1;
+	} else if (p == 1)
+	{
+		result = n;
+	} else
+	{
+		while (p != 0)
+		{
+			result *= n;
+			p--;
+		}
+	}
+
+	return (result);
+}
+
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: the dicmal string will converted
+ *
+ * Return: Always the converted dicmal number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, pow_is;
+	unsigned int result = 0;
+
+	i = 0;
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+
+	i--;
+	pow_is = 0;
+	while (i >= 0)
+	{
+		if (b[i] != '0')
+		{
+			result += clc_pow(2, pow_is);
+		}
+		pow_is++;
+		i--;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _MAIN_H_ */
